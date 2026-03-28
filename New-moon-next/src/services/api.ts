60b58b6095ff9e5906a5e71b937b3fc70053213e
flafaboodeggg-eng <flaf.aboodeggg@gmail.com>
2026-03-28 05:09:56 +0300

    Update api.ts

--- New-moon-next/src/services/api.ts
+++ New-moon-next/src/services/api.ts
@@ -3,7 +3,7 @@ const getApiBaseUrl = () => {
   if (envUrl && envUrl !== 'undefined' && envUrl !== '') {
     return envUrl;
   }
-  return 'https://c-production-fba1.up.railway.app';
+  return 'https://backend-moon-lilac.vercel.app';
 };
 
 const API_BASE_URL = getApiBaseUrl();
